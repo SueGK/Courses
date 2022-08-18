36f1c90c5831b6b5face0fc01007339c6794f258
SueGK <71711489+SueGK@users.noreply.github.com>
2022-08-18 01:37:04 +0100

    Update detect_faces_video.py

--- pyimagesearch-opencv-17-day-course/Day1-Face_detection_with_OpenCV_and_deep_learning/detect_faces_video.py
+++ pyimagesearch-opencv-17-day-course/Day1-Face_detection_with_OpenCV_and_deep_learning/detect_faces_video.py
@@ -1,6 +1,3 @@
-# USAGE
-# python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
-
 # import the necessary packages
 import os
 from imutils.video import VideoStream
