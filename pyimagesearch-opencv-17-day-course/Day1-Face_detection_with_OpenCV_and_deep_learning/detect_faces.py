54662ee19099bd8d846df34eb97bdba1f7bd4a42
SueGK <71711489+SueGK@users.noreply.github.com>
2022-08-18 01:37:22 +0100

    Update detect_faces.py

--- pyimagesearch-opencv-17-day-course/Day1-Face_detection_with_OpenCV_and_deep_learning/detect_faces.py
+++ pyimagesearch-opencv-17-day-course/Day1-Face_detection_with_OpenCV_and_deep_learning/detect_faces.py
@@ -1,6 +1,3 @@
-# USAGE
-# python detect_faces.py --image rooster.jpg --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel
-
 # import the necessary packages
 import os
 import numpy as np
